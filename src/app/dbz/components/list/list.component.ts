d8ef38243afed9f678ad3a275befd8df580e78cc
Laptop Krz182 <cristof.vergara@gmail.com>
2024-08-05 18:18:16 -0500

    curso video 72

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    id:'0',
+    name:'Trunks',
+    power:10
+  }]
+
+  @Output()
+  public onDeleteId : EventEmitter<string> = new EventEmitter();
+
+
+  onDeleteCharacter(id:string):void{
+
+    this.onDeleteId.emit(id)
+  }
+
+}
---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { v4 as uuid } from 'uuid';
+import { Character } from '../interfaces/character.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbzService {
+
+
+
+  public charaters: Character[] = [
+    {
+      id:uuid(),
+      name:'Krillin',
+      power:1000
+    },
+    {
+      id:uuid(),
+      name:'Goku',
+      power:9500
+    },
+    {
+      id:uuid(),
+      name:'Vegeta',
+      power:7500
+    }
+  ];
+
+  public onNewCharacter(character:Character):void{
+    console.log(character);
+    this.charaters.push(character);
+  }
+
+  // public onDeleteCharacter(index:number):void{
+  //   this.charaters.splice(index,1)
+  // }
+  public onDeleteCharacterById(id:string):void{
+    this.charaters = this.charaters.filter(character => character.id !== id);
+  }
+
+  constructor() { }
+}
